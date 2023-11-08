04928ec602cfd2f97362039e238ab7a23ab9052a
Masaru Fukazawa <upw.ponsuke@gmail.com>
2023-11-09 00:49:25 +0900

    挿入ソート実装

--- src/sort.ts
+++ src/sort.ts
@@ -3,7 +3,7 @@
  * @param a_list: number[]
  * @returns number[]
  */
-function bubble_sort (a_list: number[]): number[] {
+function bubble_sort(a_list: number[]): number[] {
 
     const list_num: number = a_list.length - 1
 
@@ -18,4 +18,37 @@ function bubble_sort (a_list: number[]): number[] {
     return a_list
 }
 
-export { bubble_sort }
+
+/**
+ * @name 挿入ソート
+ * @param a_list: number[]
+ * @returns number[]
+ */
+function insertion_sort(a_list: number[]): number[] {
+
+    const list_num: number = a_list.length
+
+    for(let i: number = 1; i < list_num; i++) {
+
+        const target_num: number = a_list[i]
+
+        let j: number = i
+
+        for ( ; j > 0 ; j--) {
+
+            if(a_list[j - 1] > target_num) {
+                a_list[j] = a_list[j - 1]
+            } else {
+                break
+            }
+
+        }
+
+        a_list[j] = target_num
+
+    }
+
+    return a_list
+}
+
+export { bubble_sort, insertion_sort }
--- test/sort.test.ts
+++ test/sort.test.ts
@@ -1,6 +1,11 @@
-import { bubble_sort } from '../src/sort'
+import { bubble_sort, insertion_sort } from '../src/sort'
 
 // バブルソート テスト
-test('sort [5, 4, 3, 2, 1] to [1, 2, 3, 4, 5]', () => {
+test('bubble sort [5, 4, 3, 2, 1] to [1, 2, 3, 4, 5]', () => {
     expect(bubble_sort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
+});
+
+// 挿入ソート テスト
+test('insertion sort [5, 4, 3, 2, 1] to [1, 2, 3, 4, 5]', () => {
+    expect(insertion_sort([5, 4, 3, 2, 1])).toEqual([1, 2, 3, 4, 5]);
 });

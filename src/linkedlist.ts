adeee085204997ab51d9b26d2de1d194b7d6c365
Masaru Fukazawa <upw.ponsuke@gmail.com>
2023-12-10 21:27:55 +0900

    順方向リスト作成

--- src/linkedlist.ts
+++ src/linkedlist.ts
@@ -95,11 +95,73 @@ class LinkedList {
 
         if (!this._head) return
 
-        let previous: Node | null = this.head
-        let current: Node | null | undefined = this.head?.next
+        let previous: Node | null = this._head
+        let current: Node | null | undefined = this._head?.next
+
+        if (previous && current) {
+
+            while (current) {
+
+                if (current.data == target) {
+                    previous.next = current.next as Node
+                }
+
+                previous = current
+                current = current.next
+            }
+
+        }
+
+        if (this._head?.data == target) {
+            this._head = this._head.next
+        }
 
     }
 
+    public reverse() {
+
+        if (!this._head) return
+        if (!this._head.next) return 
+
+        let current: Node | null | undefined = this._head
+        let next: Node | null = current.next
+        let previous: Node | null = null
+
+        while (current) {
+
+            next = current.next
+            current.next = previous as Node
+            previous = current
+            current = next
+            
+        }
+    
+        this._head = previous
+    }
+
+    public detect_cycle(): boolean {
+
+        if (!this._head) return false
+
+        let slow: Node = this._head
+        let fast: Node = this._head
+
+        while (true) {
+
+            try{
+                slow = slow.next as Node
+                fast = fast.next?.next as Node
+            } catch (error) {
+                return false
+            }
+
+            if (slow == fast) {
+                return true
+            }
+
+        }
+
+    }
 }
 
 export { LinkedList }
--- test/linkedlist.test.ts
+++ test/linkedlist.test.ts
@@ -27,3 +27,50 @@ test('順方向リストの検索', () => {
     expect(linkedlist.search(100)).toEqual(true);
 
 });
+
+test('順方向リストから要素を削除する', () => {
+
+    const linkedlist: LinkedList = new LinkedList();
+
+    linkedlist.append(100)
+    linkedlist.append(200)
+    linkedlist.append(300)
+    linkedlist.append(400)
+    linkedlist.append(500)
+
+    linkedlist.remove(300)
+    linkedlist.remove(100)
+
+    expect(linkedlist.datas).toEqual([200, 400, 500]);
+
+});
+
+test('順方向リストから逆順にする', () => {
+
+    const linkedlist: LinkedList = new LinkedList();
+
+    linkedlist.append(100)
+    linkedlist.append(200)
+    linkedlist.append(300)
+    linkedlist.append(400)
+    linkedlist.append(500)
+
+    linkedlist.reverse()
+
+    expect(linkedlist.datas).toEqual([500, 400, 300, 200, 100]);
+
+});
+
+test('リストが循環しているか', () => {
+
+    const linkedlist: LinkedList = new LinkedList();
+
+    linkedlist.append(100)
+    linkedlist.append(200)
+    linkedlist.append(300)
+    linkedlist.append(400)
+    linkedlist.append(500)
+
+    expect(linkedlist.detect_cycle()).toEqual(false);
+
+});
